cd16430bb21431499f7a48d210baf8b0938542a7
Raffles Gun <raffles.gun@nintex.com>
2018-05-17 10:48:28 +0800

    Update nuget packages and .Net to 4.6.2

--- NArrangeVS/Properties/AssemblyInfo.cs
+++ NArrangeVS/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("MIRATECH")]
 [assembly: AssemblyProduct("NArrangeVS")]
-[assembly: AssemblyCopyright("Copyright 2015 MIRATECH")]
+[assembly: AssemblyCopyright("Copyright 2018 MIRATECH")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
@@ -27,4 +27,4 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.4.0.*")]
+[assembly: AssemblyVersion("1.5.0.*")]
